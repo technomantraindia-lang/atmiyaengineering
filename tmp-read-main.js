ca5b23bc554f15a66a94b26a1e15ce09de4c4051
technomantraindia-lang <technomantra@gmail.com>
2026-08-21 16:26:38 +0530

    Remove temporary dev file

--- tmp-read-main.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('assets/js/main.js', 'utf8').split(/\r?\n/);
-function dump(a, b) {
-  console.log('===== ' + a + '-' + b + ' =====');
-  for (let i = a - 1; i < b && i < lines.length; i++) {
-    console.log((i + 1) + '| ' + lines[i]);
-  }
-}
-dump(1000, 1100);
